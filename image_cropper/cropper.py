68a5e176dfa13c02bb288a107145ba1815a772e6
Arata Higashiguchi <ham46903@gmail.com>
2023-10-21 16:22:43 +0900

    fix:比率を維持したまま余白ができないようにresizeしてからトリミングするよう修正

--- image_cropper/cropper.py
+++ image_cropper/cropper.py
@@ -9,14 +9,26 @@ def crop_center(image_path, output_path, size):
     :param size: 新しいサイズのための(幅、高さ)のタプル
     """
     with Image.open(image_path) as img:
-        width, height = img.size
-        new_width, new_height = size
+        img_width, img_height = img.size
+        target_width, target_height = size
 
-        left = (width - new_width)/2
-        top = (height - new_height)/2
-        right = (width + new_width)/2
-        bottom = (height + new_height)/2
+        # 画像のアスペクト比を維持しながら、指定されたサイズにリサイズ
+        if img_width / img_height >= target_width / target_height:
+            # 画像の幅がターゲットのアスペクト比よりも広い場合
+            new_height = target_height
+            new_width = int(img_width * (target_height / img_height))
+        else:
+            # 画像の高さがターゲットのアスペクト比よりも高い場合
+            new_width = target_width
+            new_height = int(img_height * (target_width / img_width))
 
-        # Crop the center of the image
+        img = img.resize((new_width, new_height), Image.LANCZOS)
+
+        # 新しいサイズでの画像の中心をトリミング
+        left = (new_width - target_width) / 2
+        top = (new_height - target_height) / 2
+        right = (new_width + target_width) / 2
+        bottom = (new_height + target_height) / 2
+
         img_cropped = img.crop((left, top, right, bottom))
         img_cropped.save(output_path)
